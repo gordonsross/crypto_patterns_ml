151a160f6bbbf6880f2d737d65d02e8b16ff3a5f
gordonsross <gordonsross@gmail.com>
2020-01-23 17:04:24 +0000

    Addition of jupyter notebook

--- factory/ml_factory.py
+++ factory/ml_factory.py
@@ -2,43 +2,28 @@
 import csv
 import logging
 import pandas as pd
-import requests
-import coinmetrics
-from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 
-from crypto_patterns_ml import coinmetrics_bridge
+from sklearn.preprocessing import MinMaxScaler
 
+from crypto_patterns_ml.data import coinmetrics_bridge
 
 log = logging.getLogger(__name__)
 
-def load_data_new():
-    cm = coinmetrics.Community()
-    supported_assets = cm.get_supported_assets()
-    print("supported assets:\n", supported_assets)
-
-    asset = "btc"
-    available_data_types = cm.get_available_data_types_for_asset(asset)
-    print("available data types:\n", available_data_types)
-
-def load_df(assets):
-    asset = 'btc'
-    metric = 'PriceUSD,BlkCnt'
-    start_date = '2019-01-01'
-    end_date = '2020-01-01'
-
-    cm = coinmetrics.Community()
-    asset_data = cm.get_asset_data_for_time_range(asset, metric, start_date, end_date)
-    return coinmetrics.cm_to_pandas(asset_data)    
+def load_data(assets, cm_metric, start_date, end_date):
+    return coinmetrics_bridge.get_asset_prices(assets, 'PriceUSD', start_date, end_date)
 
 def ml_analysis():
-    df = load_df('btc')
-    print(df.head())
-
+    pass    
 
 def main():
-    ml_analysis()    
+    assets = ['btc', 'ada', 'xrp']
+    cm_metric = 'PriceUSD'
+    start_date = '2019-01-01'
+    end_date = '2020-01-22'
+    df = load_data(assets, cm_metric, start_date, end_date)
+    print(df.head())
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     main()
     
